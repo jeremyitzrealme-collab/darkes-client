40443039b65f4bf6b9570508dc118a0397c1167b
jeremyitzrealme-collab <jeremyitzrealme@gmail.com>
2026-06-15 11:17:10 +0200

    Add Gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev/")
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "darkes-client"
